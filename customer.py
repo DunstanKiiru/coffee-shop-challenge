7ea4ff7113f409a79705ca90b97b24364040003c
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-05-19 13:36:57 +0300

    Initializes customer.py

--- customer.py
+++ customer.py
@@ -1,1 +1,44 @@
 
+class Customer:
+    all_customers = []
+
+    def __init__(self,name):
+        self.name = name
+        self._orders = []
+        Customer.all_customers.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters")
+    
+    def orders(self):
+        return self._orders
+    
+    def coffees(self):
+        return list({order.coffee for order in self._orders})
+    
+    def create_orderr(self, coffee, price):
+        from order import order
+        order = order(self, coffee, price)
+        self._orders.append(order)
+        coffee._orders.append(order)
+        return order
+    
+    @classmethod
+
+    def most_aficionado(cls, coffee):
+        aficionado = None
+        highest_spent = 0
+        for customer in cls.all_customers:
+            total = sum(order.price for order in customer.orders() if order.coffee == coffee)
+            if total > highest_spent:
+                highest_spent = total
+                aficionado = customer
+        return aficionado
