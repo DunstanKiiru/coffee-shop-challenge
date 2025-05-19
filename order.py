89ee85e3914573c5c70cb1af01e03e31d46edac8
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-05-19 13:47:59 +0300

    Initializes order.py

--- order.py
+++ order.py
@@ -1,1 +1,27 @@
 
+class Order:
+    def __init__(self, customer, coffee, price):
+        from customer import Customer
+        from coffee import Coffee
+
+        if not isinstance(customer, Customer):
+            raise TypeError("Customer must be an instance of Customer")
+        if not isinstance(coffee, Coffee):
+            raise TypeError("Coffee must be an instance of Coffee")
+        if not isinstance(price, float) or not (1.0 <= price <=10.0):
+            raise TypeError("Price must be a float between 1.0 and 10.0")
+        
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+
+    @property
+    def customer(self):
+        return self._customer
+    
+    @property
+    def coffee(self):
+        return self._coffee
+    @property
+    def price(self):
+        return self._price
