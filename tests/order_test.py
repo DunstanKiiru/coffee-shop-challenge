6e4325b11d6bdeb93a4f5ebbd7218adaf359c921
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-05-19 13:53:17 +0300

    writes order_test.py

--- tests/order_test.py
+++ tests/order_test.py
@@ -1,1 +1,21 @@
 
+import pytest
+from order import Order
+from customer import Customer
+from coffee import Coffee
+
+def test_order_creation_and_properties():
+    c = Customer("Liz")
+    coffee = Coffee("Macchiato")
+    order = Order(c, coffee, 5.5)
+    assert order.customer == c
+    assert order.coffee == coffee
+    assert order.price == 5.5
+
+def test_order_validations():
+    c = Customer("Dan")
+    coffee = Coffee("FlatWhite")
+    with pytest.raises(ValueError):
+        Order(c, coffee, 0.5)
+    with pytest.raises(TypeError):
+        Order("NotCustomer", coffee, 4.0)
